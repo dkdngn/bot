938e5af3e785754baa06ae7c597c0eb390a73047
dkdngn <66894975+dkdngn@users.noreply.github.com>
2020-07-01 10:44:32 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -2584,6 +2584,10 @@ while True:
 	async def brother5_(ctx):
 		await PlaySound(voice_client1, './sound/카운터.mp3')
 	
+	@client.command(name='!냥이')
+	async def brother5_(ctx):
+		await PlaySound(voice_client1, './sound/냥이.mp3')
+	
 	@client.command(name='!TJ', aliases=['!tj'])
 	async def TJ_(ctx):
 		resultTJ = random.randrange(1,9)
