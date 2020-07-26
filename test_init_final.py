6e85dd4148f4dc2a2c5b9706aa76d915dc479f65
dkdngn <66894975+dkdngn@users.noreply.github.com>
2020-07-26 20:45:55 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -1665,16 +1665,16 @@ while True:
 			separate_money = []
 			separate_money = msg.split(" ")
 			num_sep = floor(int(separate_money[0]))
-			cal_tax1 = floor(float(separate_money[1])*0.05)
+			cal_tax1 = floor(float(separate_money[1])*0.20)
 			
 			real_money = floor(floor(float(separate_money[1])) - cal_tax1)
-			cal_tax2 = floor(real_money/num_sep) - floor(float(floor(real_money/num_sep))*0.95)
+			cal_tax2 = floor(real_money/num_sep) - floor(float(floor(real_money/num_sep))*0.03)
 			if num_sep == 0 :
 				await ctx.send('```분배 인원이 0입니다. 재입력 해주세요.```', tts=False)
 			else :
 				embed = discord.Embed(
 					title = "----- 분배결과! -----",
-					description= '```세금 : ' + str(cal_tax1) + '\n 수령액 : ' + str(real_money) + '\n분배자 거래소등록금액 : ' + str(floor(real_money/num_sep)) + '```',
+					description= '```길드세금 : ' + str(cal_tax1) + '\n1차 수령액 : ' + str(real_money) + '\n분배자 거래소등록금액 : ' + str(floor(real_money/num_sep)) + '\n2차 세금 : ' + str(cal_tax2) + '\n인당 실수령액 : ' + str(floor(float(floor(real_money/num_sep)) + '```',
 					color=0xff00ff
 					)
 				await ctx.send(embed=embed, tts=False)
